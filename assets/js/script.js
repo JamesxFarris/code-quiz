01ba37911f74f8a4136338d9108891360188137a
James Farris <jafarris@coastal.edu>
2023-07-07 00:21:02 -0400

    Committed base files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Call the question id's
+// Call the button id's
+
+// Timer Function
+
+// Need a function for the start quiz button
+
+// Function to detect whether answer was correct
+
+// Highscore enter screen
+
+// Leaderboard
